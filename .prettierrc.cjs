86e08da6af88cc902e0002ea9ceb88fee90df1f8
Murilo Messias de Camargo <murilo.messias@gmail.com>
2024-06-15 18:10:27 -0300

    chore: add husky & lintstaged

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,45 +1,45 @@
 /** @type {import("prettier").Options} */
 module.exports = {
-  arrowParens: 'always',
-  bracketSameLine: false,
-  bracketSpacing: true,
-  embeddedLanguageFormatting: 'auto',
-  endOfLine: 'lf',
-  htmlWhitespaceSensitivity: 'css',
-  insertPragma: false,
-  jsxSingleQuote: false,
-  printWidth: 80,
-  proseWrap: 'always',
-  quoteProps: 'as-needed',
-  requirePragma: false,
-  semi: false,
-  singleAttributePerLine: false,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'all',
-  useTabs: true,
-  overrides: [
-    {
-      files: ['**/package.json'],
-      options: {
-        useTabs: false,
-      },
-    },
-    {
-      files: ['**/*.mdx'],
-      options: {
-        proseWrap: 'preserve',
-        htmlWhitespaceSensitivity: 'ignore',
-      },
-    },
-    {
-      files: ['*.astro'],
-      options: {
-        parser: 'astro',
-      },
-    },
-  ],
-  plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-astro'],
-  tailwindAttributes: ['class', 'className', 'ngClass', '.*[cC]lassName'],
-  tailwindFunctions: ['clsx', 'cn'],
+	arrowParens: 'always',
+	bracketSameLine: false,
+	bracketSpacing: true,
+	embeddedLanguageFormatting: 'auto',
+	endOfLine: 'lf',
+	htmlWhitespaceSensitivity: 'css',
+	insertPragma: false,
+	jsxSingleQuote: false,
+	printWidth: 80,
+	proseWrap: 'always',
+	quoteProps: 'as-needed',
+	requirePragma: false,
+	semi: false,
+	singleAttributePerLine: false,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'all',
+	useTabs: true,
+	overrides: [
+		{
+			files: ['**/package.json'],
+			options: {
+				useTabs: false,
+			},
+		},
+		{
+			files: ['*.mdx'],
+			options: {
+				proseWrap: 'preserve',
+				htmlWhitespaceSensitivity: 'ignore',
+			},
+		},
+		{
+			files: ['*.astro'],
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+	plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-astro'],
+	tailwindAttributes: ['class', 'className', 'ngClass', '.*[cC]lassName'],
+	tailwindFunctions: ['clsx', 'cn'],
 }
