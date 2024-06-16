7427b1de9abaf65e7c7818d79f4b596e2bcc56ac
Murilo Messias de Camargo <murilo.messias@gmail.com>
2024-06-16 08:51:57 -0300

    chore: add cloud config

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -3,7 +3,10 @@ import { config, fields, collection } from '@keystatic/core'
 
 export default config({
 	storage: {
-		kind: 'local',
+		kind: 'cloud',
+	},
+	cloud: {
+		project: 'murilo-messias/astro-with-keystatic',
 	},
 	collections: {
 		posts: collection({
