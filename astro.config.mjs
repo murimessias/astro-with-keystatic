318715bc36318486f47e72a7a5596239ba096a7d
Murilo Messias de Camargo <murilo.messias@gmail.com>
2024-06-16 08:39:01 -0300

    chore: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import keystatic from '@keystatic/astro';
 import markdoc from '@astrojs/markdoc';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [keystatic(), markdoc(), react(), tailwind(), mdx()],
-  output: 'hybrid'
+  output: 'hybrid',
+  adapter: netlify()
 });
