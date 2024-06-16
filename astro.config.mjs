1b6a0ceb68b0070f8e5d28eee96e1f90222c1761
Murilo Messias de Camargo <murilo.messias@gmail.com>
2024-06-16 09:52:48 -0300

    chore: add mdx config & kitchen sync example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
-import { defineConfig } from 'astro/config';
-import keystatic from '@keystatic/astro';
-import markdoc from '@astrojs/markdoc';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config'
+import keystatic from '@keystatic/astro'
+import markdoc from '@astrojs/markdoc'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 
-import netlify from "@astrojs/netlify";
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [keystatic(), markdoc(), react(), tailwind(), mdx()],
-  output: 'hybrid',
-  adapter: netlify()
-});
+	integrations: [keystatic(), markdoc(), react(), tailwind(), mdx()],
+	output: 'hybrid',
+	markdown: {
+		shikiConfig: {
+			theme: 'catppuccin-macchiato',
+		},
+	},
+	adapter: netlify(),
+})
