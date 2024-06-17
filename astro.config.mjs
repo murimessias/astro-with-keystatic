9215cb6ec25852b8c0ac448edf82398c06d32179
Murilo Messias de Camargo <murilo.messias@gmail.com>
2024-06-17 19:06:35 -0300

    chore: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
-import { defineConfig } from 'astro/config'
-import keystatic from '@keystatic/astro'
-import markdoc from '@astrojs/markdoc'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import keystatic from '@keystatic/astro';
+import markdoc from '@astrojs/markdoc';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify';
 
-import netlify from '@astrojs/netlify'
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mm-astro-keystatic.netlify.app/',
-	integrations: [keystatic(), markdoc(), react(), tailwind(), mdx()],
-	output: 'hybrid',
-	markdown: {
-		shikiConfig: {
-			theme: 'catppuccin-macchiato',
-		},
-	},
-	adapter: netlify(),
-})
+  site: 'https://mm-astro-keystatic.netlify.app/',
+  integrations: [keystatic(), markdoc(), react(), tailwind(), mdx(), sitemap()],
+  output: 'hybrid',
+  markdown: {
+    shikiConfig: {
+      theme: 'catppuccin-macchiato'
+    }
+  },
+  adapter: netlify()
+});
